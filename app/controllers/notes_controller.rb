aab449fc859e9b5dde58a463656649bcf4134f36
Emily Pena <emilyapena@gmail.com>
2020-02-18 15:50:28 -0700

    controller updates

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,13 @@
 class NotesController < ApplicationController
   def index
+    @notes = Note.all
   end
 
   def show
+    @notes = Note.find(params[:id])
   end
 
   def new
+    @notes = Notes.new
   end
 end
